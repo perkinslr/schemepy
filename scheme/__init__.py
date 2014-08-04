f55c7cf553f2b070e6780432fbd1f720880732c9
Logan Perkins <perkins@github.alestan.publicvm.com>
2014-08-03 18:43:38 -0700

    Added all modules to scheme default imports

---
+++ scheme/__init__.py
@@ -0,0 +1,18 @@
+import begin
+from . import builtins
+import callcc
+import case
+import cond
+import define
+import environment
+import eval
+import Globals
+import IF
+import Lambda
+import macro
+import parser
+import procedure
+import processer
+import symbol
+import token
+import utils
