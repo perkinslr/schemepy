0d721ee9825375ea141ee52a88a8f5c296104638
Logan Perkins <lperkins2@zagmail.gonzaga.edu>
2014-10-22 15:50:21 -0700

    Added (for ...) macro

--- scheme/builtins.scm
+++ scheme/builtins.scm
@@ -3,6 +3,8 @@
         (define ret (callable (car lst)))
         (if (cdr lst) (for-each callable (cdr lst)) ret))
     
+    (defmacro (for x in l . calls) (for-each (lambda (x) ,@calls) ,l))
+    
     (define-macro and (lambda args 
         (if (null? args) #t
             (if (= (length args) 1) (car args)
